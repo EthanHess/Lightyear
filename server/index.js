da94fb9b38b0ea358e972785d69b32d23d51e24c
EthanHess <ech1988@gmail.com>
2018-10-30 13:30:26 -0700

    images and news fetching

--- server/index.js
+++ server/index.js
@@ -1,10 +1,18 @@
 const massive = require('massive');
 const axios = require('axios');
-// const express = require('express');
-// const bodyPaser = require('body-parser');
-// const session = require('express-session');
+const express = require('express');
+const bodyPaser = require('body-parser');
+//const session = require('express-session');
+
+const app = express();
 
 require('dotenv').config();
 //massive(process.env.CONNECTION_STRING).then(db => app.set('db', db));
 
 //Networking
+
+//Listen
+const SERVER_PORT = process.env.SERVER_PORT;
+app.listen(SERVER_PORT, () => {
+  console.log('Server listening on port ' + SERVER_PORT);
+});
--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import axios from 'axios'; 
 import logo from './Spacegram-logo-one.png'; 
+import astronaut from './astronaut.png'; 
+import news from './news.png'; 
+import friends from './friends.png'; 
 import './App.css';
 
 class App extends Component {
@@ -37,13 +40,14 @@ class App extends Component {
     this.addListener() 
     // --- TODO credit Google for their API! ---
 
-    console.log('process', process.env.GOOGLE_NEWS_API_KEY); 
+    console.log('process env', process.env); 
+    console.log('process', process.env.REACT_APP_GOOGLE_NEWS_API_KEY); 
 
     var url = 'https://newsapi.org/v2/everything?' +
     'q=Nasa&' +
     'from=2018-10-29&' +
     'sortBy=popularity&' +
-    `apiKey=${process.env.GOOGLE_NEWS_API_KEY}`;
+    `apiKey=${process.env.REACT_APP_GOOGLE_NEWS_API_KEY}`;
 
     axios.get(url).then(response => {
       console.log('response.data', response.data); 
@@ -52,7 +56,7 @@ class App extends Component {
     })
   }
 
-  componentDidUnmount() {
+  componentWillUnmount() {
     this.removeListener()
   }
 
@@ -66,15 +70,29 @@ class App extends Component {
          </div>
         </header>
         <main className="main_body">
-          <div className="space_animation_container">
+          <div className="intro_container">
               <div className="text_intro">
                 <p>An online community for Astronomy geeks</p>
               </div>
-              <div className="space_animation">
-                <div className="news_bubble"></div>
-                <div className="friends_bubble"></div>
-                <div className="space_station_bubble"></div>
+              <div className="intro_tabs">
+                <div className="news_bubble">
+                    <img className="news_img" src={news} alt=""/>
+                    <p>News</p>
+                </div>
+                <div className="friends_bubble">
+                    <img className="friends_img" src={friends} alt=""/>
+                    <p>Friends</p>
+                </div>
+                <div className="space_station_bubble">
+                    <img className="ss_img" src={astronaut} alt=""/>
+                    <p>Space Station</p>
+                </div>
               </div>
+          </div>
+          <div className="space_animation_container">
+            <div className="space_animation">
+
+            </div>
           </div>
         </main>
       </div>
