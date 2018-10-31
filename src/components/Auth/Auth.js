ceeafe1fb75209f6e3c58f5b072338df756cc32e
EthanHess <ech1988@gmail.com>
2018-10-31 12:28:05 -0700

    Auth portal setup

--- src/components/Auth/Auth.js
+++ src/components/Auth/Auth.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react'; 
+// import axios from 'axios'; 
+import './Auth.css'; 
+import noUser from './no_user.png'; 
 
 export default class Auth extends Component {
     constructor() {
@@ -6,7 +9,19 @@ export default class Auth extends Component {
     }
     render() {
         return (
-            <div>Auth</div>
+            <div className="auth_container">
+                <div className="auth_bar">
+                    <p>Create an account or sign in with a 3rd party</p>
+                </div>
+                <div className="auth_portal">
+                    <img src={noUser} alt=""/>
+                    <input placeholder="Username"></input>
+                    <input placeholder="Password"></input>
+                    <input placeholder="Email"></input>
+                    <button>Proceed</button>
+                    <button>3rd Party (Facebook etc.)</button>
+                </div>
+            </div>
         )
     }
 }
--- src/components/News/News.js
+++ src/components/News/News.js
@@ -2,6 +2,10 @@ import React, { Component } from 'react';
 import axios from 'axios'; 
 import './News.css'; 
 
+import search from './search.png'; 
+import space from './space.png'; 
+import video from './video.png'; 
+
 export default class News extends Component {
     constructor() {
         super()
@@ -10,6 +14,37 @@ export default class News extends Component {
             searchString: '', 
             videoMode: false
         }
+    }
+
+    handleInput = (val) => {
+        this.setState({ searchString: val})
+    }
+
+    //TODO: have only one button? 
+    switchMode = () => {
+        this.setState({ videoMode: !this.state.videoMode})
+    }
+
+    archive = (val) => {
+
+    }
+
+    share = (val) => {
+
+    }
+
+    searchTapped = () => {
+
+    }
+
+    //Can maybe DRY and fetch both in same function? 
+    fetchYoutubeVideos() {
+        var url = ''
+        axios.get(url).then(response => {
+
+        }).catch(error => {
+            
+        })
     }
 
     componentDidMount() {
@@ -25,18 +60,34 @@ export default class News extends Component {
            console.log('error fetching news data', error); 
         })
     }
+
     render() {
         console.log('this.state.mainNF', this.state.mainNewsFeed)
         const mainFeedMapped = this.state.mainNewsFeed.map(newsItem => {
             return <div className="feed_cell">
                 <p>{newsItem.title}</p>
+                <img src={newsItem.urlToImage} alt=""/>
+                <div>
+                    <button>Archive</button>
+                    <button>Share</button>
+                </div>
             </div>
         })
 
         return (
             <div className="news_container">
                 <div className="top_bar">
-                
+                    <div className="search_div">
+                        <img src={search} alt=""/>
+                        <input placeholder=" Search" onChange={(e) => this.handleInput(e.target.value)}></input>
+                        <button>Search</button>
+                    </div>
+                    <div className="type_div">
+                        <img src={space} alt=""/>
+                        <button>News</button>
+                        <img src={video} alt=""/>
+                        <button>Videos</button>
+                    </div>
                 </div>
                 <div className="news_table">
                     { mainFeedMapped }
